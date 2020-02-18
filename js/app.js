1cbfc94c364c94bfff602c32edfeaca915193011
amal almomani <amalalmomani19@gmail.com>
2020-02-18 12:19:22 +0200

    change

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@ switch (ques1.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("No your are wrong , I hate all kind af animels except hourses.");
-        console.log("Do you think that I like animals ?", ques1);
+        //console.log("Do you think that I like animals ?", ques1);
         break;
     case 'no':
     case 'n':
         alert("You are right!");
-        console.log("Do you think that I like animals ?", ques1);
+        //console.log("Do you think that I like animals ?", ques1);
         break;
     default:
         alert("please, try again! answer with yes or no to see the answer.");
-        console.log("Do you think that I like animals ?", ques1);
+        //console.log("Do you think that I like animals ?", ques1);
         break;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,16 +28,16 @@ switch (ques2.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Pravo!");
-        console.log("Do you think that I like healthy food?", ques2);
+        //console.log("Do you think that I like healthy food?", ques2);
         break;
     case 'no':
     case 'n':
         alert("Uhh, I like it so much");
-        console.log("Do you think that I like healthy food?", ques2);
+        //console.log("Do you think that I like healthy food?", ques2);
         break;
     default:
         alert("please, try again! answer with yes or no to see the answer.");
-        console.log("Do you think that I like healthy food?", ques2);
+        //console.log("Do you think that I like healthy food?", ques2);
         break;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,16 +47,16 @@ switch (ques3.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Pravo!, I like it a lot");
-        console.log("Do you think that I like coffee ?", ques3);
+        //console.log("Do you think that I like coffee ?", ques3);
         break;
     case 'no':
     case 'n':
         alert("You are wrong! , I am a coffee addict");
-        console.log("Do you think that I like coffee ?", ques3);
+        //console.log("Do you think that I like coffee ?", ques3);
         break;
     default:
         alert("please, try again! answer with yes or no to see the answer.");
-        console.log("Do you think that I like coffee ?", ques3);
+        //console.log("Do you think that I like coffee ?", ques3);
         break;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,16 +66,16 @@ switch (ques4.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Yes, you know me a lot");
-        console.log("Do you think that I like draw ? ", ques4);
+        //console.log("Do you think that I like draw ? ", ques4);
         break;
     case 'no':
     case 'n':
         alert("Sorry you are fail");
-        console.log("Do you think that I like draw ? ", ques4);
+        //console.log("Do you think that I like draw ? ", ques4);
         break;
     default:
         alert("please, try again! answer with yes or no to see the answer.");
-        console.log("Do you think that I like draw ? ", ques4);
+        //console.log("Do you think that I like draw ? ", ques4);
         break;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,16 +84,16 @@ switch (ques5.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("No. I dont mind.");
-        console.log("Do you think that I like cars?", ques5);
+        //console.log("Do you think that I like cars?", ques5);
         break;
     case 'no':
     case 'n':
         alert("good!");
-        console.log("Do you think that I like cars?", ques5);
+        //console.log("Do you think that I like cars?", ques5);
         break;
     default:
         alert("please, try again! answer with yes or no to see the answer.");
-        console.log("Do you think that I like cars?", ques5);
+        //console.log("Do you think that I like cars?", ques5);
         break;
 }
 
