3a5eff99792c57d2d121c43fba3ca0935e53da81
amal almomani <amalalmomani19@gmail.com>
2020-02-18 16:41:11 +0200

    add loops

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Welcome to about-me
 
-It have little of information about my self , some style on css and 5 quastions on java script.
+## general information about my website:
+
+**It have little of information about my self , some style on css and 5 quastions on java script.**
--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 `use strict`;
-
+var score = 0
 var yName = prompt("What is your name ?");
 console.log("What is your name ?", yName);
-alert(`Welcome! ${yName}  ` );
+alert(`Welcome! ${yName}  `);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var ques1 = prompt("Do you think that I like animals ?");
@@ -15,6 +15,7 @@ switch (ques1.toLowerCase()) {
     case 'no':
     case 'n':
         alert("You are right!");
+        score++;
         //console.log("Do you think that I like animals ?", ques1);
         break;
     default:
@@ -28,6 +29,7 @@ switch (ques2.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Pravo!");
+        score++;
         //console.log("Do you think that I like healthy food?", ques2);
         break;
     case 'no':
@@ -47,6 +49,7 @@ switch (ques3.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Pravo!, I like it a lot");
+        score++;
         //console.log("Do you think that I like coffee ?", ques3);
         break;
     case 'no':
@@ -66,6 +69,7 @@ switch (ques4.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Yes, you know me a lot");
+        score++;
         //console.log("Do you think that I like draw ? ", ques4);
         break;
     case 'no':
@@ -89,6 +93,7 @@ switch (ques5.toLowerCase()) {
     case 'no':
     case 'n':
         alert("good!");
+        score++;
         //console.log("Do you think that I like cars?", ques5);
         break;
     default:
@@ -99,4 +104,64 @@ switch (ques5.toLowerCase()) {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-alert(`Thank you ${yName}`);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+for (var i = 0; i < 4; i++) {
+    var ques6 = prompt("Guess what number I choose ? ");
+    if (i == 3) {
+        alert("the correct answer is 55");
+        //console.log("Guess what number I choose ? ", ques6);
+        break;
+    }
+    if (ques6 == 55) {
+        alert("you are right");
+        console.log("Guess what number I choose ? ", ques6);
+        score++;
+        break;
+
+    } else if (ques6 > 55) {
+        alert("you are too high");
+        //console.log("Guess what number I choose ? ", ques6);
+    } else if (ques6 < 55) {
+        alert("you are too low");
+        //console.log("Guess what number I choose ? ", ques6);
+
+    }
+
+}
+/////////////////////////////////////////
+
+var places =['Blue lagoon','Switzerland', 'Unaited state', 'lituania','Maldives','Greece'];
+for ( var p = 0 ; p <= 6  ; p++){
+    if( p == 6  ){
+        alert("you are out of attempts! , the right answer are Blue lagoon,Switzerland, Unaited state, lituania, Maldives and Greece");
+        
+          break ;
+    }
+    //console.log(places[p]);
+    var ques7 = prompt("Guess what my favorite place ?");
+    if (ques7 === places[p]){
+        alert("you are right");
+        //console.log("Guess what my favorite place ?", ques7);
+        score++;
+        break;
+        
+    }
+
+    }
+
+
+       alert(`Thank you  ${yName} , Your score is : ${score}`);
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
